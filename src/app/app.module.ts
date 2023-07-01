995b6ab0b8ebb8f86a558053b99445da4d8fc903
MaheshNiv <mahesh.nivangune11@gmail.com>
2023-07-01 20:09:59 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DetailsComponent } from './details/details.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {HttpClientModule} from '@angular/common/http';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import { FormsModule } from '@angular/forms';
+import { Scrollbar } from 'smooth-scrollbar/scrollbar';
+import { NgxPaginationModule } from 'ngx-pagination';
+import {Ng2OrderModule} from 'ng2-order-pipe'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatCardModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    FormsModule,
+    NgxPaginationModule,
+    Ng2OrderModule
+
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
